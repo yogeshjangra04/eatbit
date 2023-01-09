a3920fa0a117fb5b0033159cb5a79c744f0fddb2
yogeshjangra04 <yogesh04012000@gmail.com>
2023-01-10 02:05:51 +0530

    api url change

--- frontend/src/apis/pizza.js
+++ frontend/src/apis/pizza.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://fooddelivery-yogesh.vercel.app/',
 });
 
 //"start": "node ./backend/server.js",
